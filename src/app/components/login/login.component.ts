dc2304f5886ac8b0717e9946a4939e65f6dc20ea
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-29 21:34:27 +0300

    created login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private api: ApiService) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.api.login('PartnerAdmins/login', this.loginForm.value).subscribe(
+      res => {
+        console.log(res);
+        localStorage.setItem('token', res.id);
+        localStorage.setItem('ttl', res.ttl);
+        this.router.navigate(['/products']);
+      },
+      err => {
+        alert(err);
+      }
+    );
+  }
+}
