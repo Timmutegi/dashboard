bbef2fa3c4237aa0538ab45a91aa3d5eeb74f94c
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-29 21:33:37 +0300

    created commissions component

---
+++ src/app/components/commissions/commissions.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from '../../services/api.service';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-commissions',
+  templateUrl: './commissions.component.html',
+  styleUrls: ['./commissions.component.scss']
+})
+export class CommissionsComponent implements OnInit {
+  isLoading = true;
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['agent', 'amount', 'paid', 'date'];
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.getCommissions();
+  }
+
+  getCommissions() {
+    const ID = localStorage.getItem('partnerProductID');
+    this.api.get(`PartnerProducts/${ID}/commissions`).subscribe(
+      res => {
+        this.isLoading = false;
+        this.dataSource.data = res;
+      }
+    );
+  }
+
+}
