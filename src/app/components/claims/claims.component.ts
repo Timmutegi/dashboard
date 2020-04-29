9250ab5e6b557a3c8ce8337701c5bdbb1999d4ae
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-04-29 21:32:16 +0300

    Created claims component

---
+++ src/app/components/claims/claims.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-claims',
+  templateUrl: './claims.component.html',
+  styleUrls: ['./claims.component.scss']
+})
+export class ClaimsComponent implements OnInit {
+  isLoading = true;
+  displayedColumns: string[] = ['status', 'processingHours', 'amount', 'benefit', 'createdAt'];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.getClaims();
+  }
+
+  getClaims() {
+    const ID = localStorage.getItem('partnerProductID');
+    this.api.get(`PartnerProducts/${ID}/claims`).subscribe(
+      res => {
+        this.isLoading = false;
+        this.dataSource.data = res;
+      }
+    );
+  }
+}
+
+
