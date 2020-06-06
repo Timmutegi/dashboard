f30dc28863366f1808066942fdb74d6b27b304b1
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-06 21:55:58 +0300

    Added brand assets

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  logo: string;
+  brandColor: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.logo = JSON.parse(localStorage.getItem('admin')).logo;
+    this.brandColor = JSON.parse(localStorage.getItem('admin')).styles.color;
   }
 
 }
