fc525b80427b0fc5276d7e894d8b72c379ad542e
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-18 12:59:38 +0300

    Added filter by date and export functionality

--- src/app/components/commissions/commissions.component.ts
+++ src/app/components/commissions/commissions.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from '../../services/api.service';
 import { MatPaginator } from '@angular/material/paginator';
+import { FormGroup, FormControl } from '@angular/forms';
+
+export interface Data {
+  createdAt: Date;
+}
+
+const ELEMENT_DATA: Data[] = [];
 
 @Component({
   selector: 'app-commissions',
@@ -10,8 +17,25 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class CommissionsComponent implements OnInit {
   isLoading = true;
-  dataSource = new MatTableDataSource();
+  minDate: Date;
+  maxDate: Date;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   displayedColumns: string[] = ['index', 'agent', 'amount', 'paid', 'date'];
+
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+  });
+
+  get fromDate() {
+    const fromDate = new Date(this.filterForm.get('fromDate').value);
+    return fromDate;
+  }
+  get toDate() {
+    const toDate = new Date(this.filterForm.get('toDate').value);
+    toDate.setDate(toDate.getDate() + 1);
+    return toDate;
+  }
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
@@ -20,21 +44,36 @@ export class CommissionsComponent implements OnInit {
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.getCommissions();
+    this.maxDate = new Date();
+    this.minDate = new Date(2019, 0, 1);
+    this.dataSource.filterPredicate = (data, filter) => {
+      if (this.fromDate && this.toDate) {
+        return data.createdAt >= this.fromDate && data.createdAt <= this.toDate;
+      }
+      return true;
+    };
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter1() {
+    this.dataSource.filter = '' + Math.random();
   }
 
   getCommissions() {
     const ID = localStorage.getItem('partnerProductID');
     this.api.get(`PartnerProducts/${ID}/commissions`).subscribe(
       res => {
+        res.forEach((element: { createdAt: string | number | Date; }) => {
+          element.createdAt = new Date(element.createdAt);
+        });
         this.isLoading = false;
         this.dataSource.data = res;
       }
     );
+  }
+
+  reset() {
+    this.filterForm.reset();
+    this.dataSource.filter = '';
   }
 
 }
