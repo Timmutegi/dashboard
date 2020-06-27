6630378acc40f682d1fb908892aef0c775f0f419
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-27 21:31:44 +0300

    updated policies

--- src/app/components/policies/policies.component.ts
+++ src/app/components/policies/policies.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { ApiService } from '../../services/api.service';
+
+export interface Data {
+  startDate: Date;
+  groupId: string;
+}
+
+const ELEMENT_DATA: Data[] = [];
 
 @Component({
   selector: 'app-policies',
@@ -6,10 +17,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./policies.component.scss']
 })
 export class PoliciesComponent implements OnInit {
+  isLoading = true;
+  minDate: Date;
+  maxDate: Date;
+  groups: any = [];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = [
+    'serial',
+    'firstname',
+    'lastname',
+    'phone',
+    'startDate',
+    'expiryDate',
+    'paid',
+    'price',
+    'nextPaymentDate',
+    'nextPaymentAmount'
+  ];
 
-  constructor() { }
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+  });
+
+  get fromDate() {
+    const fromDate = new Date(this.filterForm.get('fromDate').value);
+    return fromDate;
+  }
+  get toDate() {
+    const toDate = new Date(this.filterForm.get('toDate').value);
+    toDate.setDate(toDate.getDate() + 1);
+    return toDate;
+  }
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.getPolicies();
+    this.test();
+    this.maxDate = new Date();
+    this.minDate = new Date(2019, 0, 1);
+    this.dataSource.filterPredicate = (data, filter) => {
+      if (this.fromDate && this.toDate) {
+        return data.startDate >= this.fromDate && data.startDate <= this.toDate;
+      }
+      return true;
+    };
+  }
+
+  applyFilter1() {
+    this.dataSource.filter = '' + Math.random();
+  }
+
+  getPolicies() {
+    const ID = localStorage.getItem('partnerProductID');
+    this.api.get(`PartnerProducts/${ID}/policies?filter={"where":{"groupId": null}, "include": "policyHolder"}`).subscribe(
+      res => {
+        res.forEach((element: { startDate: string | number | Date; }) => {
+          element.startDate = new Date(element.startDate);
+        });
+        // console.log(res);
+        this.isLoading = false;
+        this.dataSource.data = res;
+      }
+    );
+  }
+
+  test() {
+    const ID = localStorage.getItem('partnerProductID');
+    this.api.get(`PartnerProducts/${ID}/policies?filter={"where":{"policyHolderId": null}, "include": "group"}`).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+  }
+
+  reset() {
+    this.filterForm.reset();
+    this.dataSource.filter = '';
   }
 
 }
