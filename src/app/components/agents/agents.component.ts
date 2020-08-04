9ce0ee3ebe37ee5b840464e98708b7ef5b80a49c
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-08-04 09:38:44 +0300

    Added Export Type

--- src/app/components/agents/agents.component.ts
+++ src/app/components/agents/agents.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { ApiService } from '../../services/api.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ExportType } from 'mat-table-exporter';
 
 export interface Data {
   createdAt: Date;
@@ -16,6 +17,7 @@ const ELEMENT_DATA: Data[] = [];
   styleUrls: ['./agents.component.scss']
 })
 export class AgentsComponent implements OnInit {
+  exportType = ExportType.XLSX;
   isLoading = true;
   minDate: Date;
   maxDate: Date;
